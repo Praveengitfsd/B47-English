cb1cbca072ae7becdf8852bb01c2a6a49dd864bc
Praveengitfsd <withmepkn@gmail.com>
2023-04-21 10:53:01 +0530

    Day03

---
+++ Daily Assessment/Day03_20042023/Test.js
@@ -0,0 +1,58 @@
+let myResume={
+    "basics": {
+      "name": "Praveen Kumar",
+      "email": "withmepkn@gmail.com",
+      "phone": 9999999999,
+      "degree": "B.E",
+      "location": {
+        "address": "XXXXXX",
+        "postalCode": 600000,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "website",
+          "github":"website",
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Good Company",
+        "position": "Good Position",
+        "startDate": "2010-05-19",
+        "endDate": "Nil",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      }
+    ],
+    "education": [
+      {
+        "institution": "Good College",
+        "department": "Good Dept",
+        "studyType": "fulltime",
+        "batch start year": 2005,
+        "batch end year": 2010,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": "automatic attendance maintaing system using python language",
+    }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
